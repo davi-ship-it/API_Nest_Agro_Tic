98e0e67f0ef14c698974a8839b094fb9a4a4b96c
davi-ship-it <pitalitodavid654@gmail.com>
2025-09-29 19:23:29 -0500

    estable

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: "http://localhost:5173",
   credentials: true
 }));
   app.use(cookieParser());
